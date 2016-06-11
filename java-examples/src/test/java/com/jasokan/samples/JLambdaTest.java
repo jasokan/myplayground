7825ac5c64e8466e73d46f44a3bf9fb1ea6ce9ab
jasokan <jasokan@github.com>
2016-06-11 22:18:48 +0530

    str test

---
+++ java-examples/src/test/java/com/jasokan/samples/JLambdaTest.java
@@ -0,0 +1,31 @@
+package com.jasokan.samples;
+
+public class JLambdaTest {
+    
+    public static void main(String[] args) {
+	
+	JLambdaTest jTest = new JLambdaTest();
+	SimpleInterestCalculation simpleInterest = (float principal, float rateOfInterest, int numOfYears) -> principal * rateOfInterest * numOfYears / 100	;
+	CompundInterestCalculation compoundInterest = (float principal, float rateOfInterest, int numOfYears, int compoundsPerYear) -> Math.pow((double)(principal * (1+rateOfInterest/compoundsPerYear)), (double)compoundsPerYear*numOfYears  );
+	
+    }
+    
+    @FunctionalInterface
+    interface SimpleInterestCalculation {
+	double simpleInterest(float principal, float rateOfInterest, int numOfYears);
+    }
+    
+    @FunctionalInterface
+    interface CompundInterestCalculation {
+	double compoundInterest(float principal, float rateOfInterest, int numOfYears, int compoundsPerYear);
+    }
+    
+    private float calculateSimple(float principal, float rateOfInterest, int numOfYears, int compoundsPerYear) {
+	return 0;
+    }
+    
+    private float calculateCompund(float principal, float rateOfInterest, int numOfYears, int compoundsPerYear) {
+	return 0;
+    }
+
+}
---
+++ java-examples/src/test/java/com/jasokan/samples/JStringTest.java
@@ -0,0 +1,38 @@
+package com.jasokan.samples;
+
+import org.perf4j.LoggingStopWatch;
+import org.perf4j.StopWatch;
+
+public class JStringTest {
+    
+    public static void main(String[] args) {
+	
+	int count = 20000;
+	
+	StopWatch stringAddition = new LoggingStopWatch();
+	String sAddition = "";
+	for(int i=0;i<count;i++) {
+	    sAddition += i;
+	}
+	//System.out.println(sAddition);
+	stringAddition.stop("String Addition", "String Addition");
+	
+	StopWatch stringBuilder = new LoggingStopWatch();
+	StringBuilder sBuilder = new StringBuilder();
+	for(int i=0;i<count;i++) {
+	    sBuilder.append(i);
+	}
+	//System.out.println(sBuilder);
+	stringBuilder.stop("String Builder", "String Builder");
+	
+	StopWatch stringBuffer = new LoggingStopWatch();
+	StringBuffer sBuffer = new StringBuffer();
+	for(int i=0;i<count;i++) {
+	    sBuffer.append(i);
+	}
+	//System.out.println(sBuffer);
+	stringBuffer.stop("String Buffer", "String Buffer");
+	
+	
+    }
+}

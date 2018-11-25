92d227e5fd9d6c72d1bbddb9d8b12a11b649af1a
jasokan <jasokan@gmail.com>
2018-11-25 12:48:29 +0530

    Lua String

--- lua-examples/jString.lua
+++ lua-examples/jString.lua
@@ -10,3 +10,20 @@ print("Upper Case  of cString  ", string.upper(cString))
 print("SubString   ", string.sub(cString, 0, 5))
 indexStart, indexEnd = string.find(cString, "Jagan")
 print(indexStart, indexEnd)
+print("global Substitution  ", string.gsub(cString, "nathan", "mohan"))
+independenceDay="India got independence on 15/08/1947, thanks"
+dateformat="%d%d/%d%d/%d%d%d%d"
+print("Independence day  ", string.sub(independenceDay, string.find(independenceDay, dateformat)))
+/*
+.	all characters
+%a	letters
+%c	control characters
+%d	digits
+%l	lower case letters
+%p	punctuation characters
+%s	space characters
+%u	upper case letters
+%w	alphanumeric characters
+%x	hexadecimal digits
+%z	the character with representation 0
+*/

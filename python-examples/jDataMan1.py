f8e5dc9916ef948ea936b089878ea562c91e02d8
Jagannathan Asokan <jasokan@gmail.com>
2018-12-23 10:43:36 +0530

    pandas example

---
+++ python-examples/jDataMan1.py
@@ -0,0 +1,10 @@
+import pandas as pandaData
+
+# installing Pandas
+# pip install numpy
+# sudo apt-get install python3-dev
+# pip install pandas
+
+jdataSeries = pandaData.read_csv("test.csv", squeeze=True)
+print(jdataSeries.count())
+print(jdataSeries.sum())
